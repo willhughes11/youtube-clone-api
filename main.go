75ff4a616d4e035968af9eccf50602907c37c44c
willhughes11 <willhughes2110@gmail.com>
2023-10-16 00:29:44 -0400

    youtube videos by channel id item object replacement functionality

--- main.go
+++ main.go
@@ -15,15 +15,27 @@ func main() {
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
 	router.Use(cors.New(config))
 
+	// Base
 	router.GET("/api/v1", getApiBaseEndpoint)
 
+	// Videos
 	router.GET("/api/v1/videos/mostPopular", getPopularYoutubeVideos)
+	router.GET("/api/v1/videos/:id", getVideoById)
 	router.GET("/api/v1/videos/channel/:id", getVideosByChannelId)
 
+	// Playlists
+	router.GET("/api/v1/playlists/:id", getPlaylistById)
+
+	// Playlist Items
+	router.GET("/api/v1/playlistItems/playlist/:playlist_id", getPlaylistItemsByPlaylistId)
+
+	// Video Categories
 	router.GET("/api/v1/videosCategories", getVideoCategoriesByRegionCode)
 
+	// Channels
 	router.GET("/api/v1/channels", getChannel)
 
+	// Channel Sections
 	router.GET("/api/v1/channelSections/channel/:id", getChannelSectionsByChannelId)
 	router.GET("/api/v1/channelSections/:id", getChannelSectionsById)
 
--- videos.go
+++ videos.go
@@ -44,7 +44,7 @@ func getPopularYoutubeVideos(c *gin.Context) {
 			return
 		}
 
-		data := processItemsConcurrently(jsonResponse, service)
+		data := processItemsConcurrently(jsonResponse, service, false)
 
 		// Marshal the modified data back into JSON
 		modifiedResponse, err := json.Marshal(data)
@@ -60,17 +60,17 @@ func getPopularYoutubeVideos(c *gin.Context) {
 	}
 }
 
-func getVideosByChannelId(c *gin.Context) {
-	videoSearchParts := strings.Split("snippet", ",")
-	channelId := c.Param("id")
+func getVideoById(c *gin.Context) {
+	videoParts := strings.Split("contentDetails,id,snippet,statistics,topicDetails", ",")
+	videoId := c.Param("id")
 
 	service, err := getGoogleApiService()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
 	}
 
-	call := service.Search.List(videoSearchParts)
-	call = call.ChannelId(channelId)
+	call := service.Videos.List(videoParts)
+	call = call.Id(videoId)
 	response, err := call.Do()
 	if err != nil {
 		log.Printf("Error making API call: %v", err)
@@ -79,4 +79,48 @@ func getVideosByChannelId(c *gin.Context) {
 	}
 
 	c.IndentedJSON(http.StatusOK, response)
+}
+
+func getVideosByChannelId(c *gin.Context) {
+	videoSearchParts := strings.Split("snippet", ",")
+	channelId := c.Param("id")
+	order := c.DefaultQuery("order", "date")
+	getVideoItemObject := c.DefaultQuery("gvio", "true")
+
+	service, err := getGoogleApiService()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+	}
+
+	call := service.Search.List(videoSearchParts)
+	call = call.ChannelId(channelId).Type("video").Order(order)
+	response, err := call.Do()
+	if err != nil {
+		log.Printf("Error making API call: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+
+	if getVideoItemObject == "true" {
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			log.Printf("Error marshaling response: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+
+		data := processItemsConcurrently(jsonResponse, service, true)
+
+		// Marshal the modified data back into JSON
+		modifiedResponse, err := json.Marshal(data)
+		if err != nil {
+			log.Printf("Error marshaling modified data: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+
+		c.Data(http.StatusOK, "application/json; charset=utf-8", modifiedResponse)
+	} else {
+		c.IndentedJSON(http.StatusOK, response)
+	}
 }
